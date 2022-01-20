95c16ee481be523f09c45156c4b0b63d40bfa8e6
Nick <nsagias@gmail.com>
2022-01-20 16:24:01 -0500

    Add possible errors with file

--- question-004/question-004.js
+++ question-004/question-004.js
@@ -1,11 +1,11 @@
-var server_echo;
+var server_echo;  // declared as var and can be  re-assigned by another function
 var json = {
     json: JSON.stringify({
         a: 1,
         b: 2
     }),
     delay: 3
-};
+}; // declared as var and can be  re-assigned by another function, naming difficult to read
 fetch('/echo/', {
     method: 'post',
     headers: {
@@ -15,7 +15,7 @@ fetch('/echo/', {
     body: 'json=' + encodeURIComponent(JSON.stringify(json.json)) + '&delay=' + json.delay
 })
 .then(function (response) {
-    server_echo = response.json().echo
+    server_echo = response.json().echo  // not a function  for javascript
     return response.json();
 })
 .then(function (result) {
@@ -24,6 +24,9 @@ fetch('/echo/', {
 .catch (function (error) {
     console.log('Request failed', error);
 });
+
+// original declaration is with out a type/interface
+// this will return and error if it is not an array[]
 server_echo.forEach(
     element => console.log(element)
 )
