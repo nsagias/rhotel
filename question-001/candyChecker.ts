855097fde18bd5b46ea807e44bde4d3b9ebb5dd9
Nick <nsagias@gmail.com>
2022-01-20 15:05:08 -0500

    all value must be void type)

--- question-001/candyChecker.ts
+++ question-001/candyChecker.ts
@@ -6,15 +6,16 @@
 // note: number is a positive integer number;
 
 interface ICandyInput {
-  (num: number): void;
+  (num: number): void
 }
 
 const candyChecker: ICandyInput = (num: number) => {
   let result: string | number = "";
+  if (typeof num !== "number") throw new Error ('runtime type checking');
   if (num < 0) return;
   if (num === 0) return result = num;
   if (num % 22 === 0) return console.log(result = "candybar"); 
   if (num % 2 === 0)  return console.log(result = "candy"); 
   if (num % 11 === 0) return console.log(result = "bar");
-  return result = num;
+  return console.log(result = num);
 };
