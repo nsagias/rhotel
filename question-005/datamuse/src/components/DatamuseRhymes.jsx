f33208c3e505c7b315a4bcc9aeb212a7b4839cfd
Nick <nsagias@gmail.com>
2022-01-24 02:13:14 -0500

    import useState, useEffect, create api link, and create starter states

--- question-005/datamuse/src/components/DatamuseRhymes.jsx
+++ question-005/datamuse/src/components/DatamuseRhymes.jsx
@@ -1,4 +1,14 @@
+import { useEffect, useState } from "react";
+
 export default function DatamuseRyhmes() {
+  const keyWord = "forgetful";
+  const [word, setWord] = useState(keyWord);
+  const [wordThatRhyme, setWordsThatRhyne] = useState([]);
+
+  const API = `https://api.datamuse.com/words?rel_rhy=${word}`;
+    useEffect(() => {
+      axios.get('')
+    }, []);
   return (
     <></>
   );
