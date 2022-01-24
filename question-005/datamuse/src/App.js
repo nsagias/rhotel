f9949cf41a19a8570af611ce2fa76e755421caa3
Nick <nsagias@gmail.com>
2022-01-24 02:06:20 -0500

    Create datamuse rhyme component

--- question-005/datamuse/src/App.js
+++ question-005/datamuse/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import DatamuseRyhmes from './components/DatamuseRhymes';
 
 function App() {
   return (
     <div className="App">
-  
+      <DatamuseRyhmes />
     </div>
   );
 }
