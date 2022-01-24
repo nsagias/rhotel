230fcbfc66eadac53c7c86ec96ff143d49416174
Nick <nsagias@gmail.com>
2022-01-24 02:40:07 -0500

    add index and assign to key

--- question-005/datamuse/src/components/DatamuseRhymes.jsx
+++ question-005/datamuse/src/components/DatamuseRhymes.jsx
@@ -21,23 +21,23 @@ const data = [
 
 export default function DatamuseRyhmes() {
   const keyWord = "forgetful";
-  // const [word, setWord] = useState(keyWord);
+  const [word, setWord] = useState(keyWord);
   const [wordsThatRhyme, setWordsThatRhyne] = useState([]);
   // console.log("keyword", keyWord);
   // console.log("word",word);
   console.log("world that rhyme",wordsThatRhyme);
 
   // https://api.datamuse.com/words?rel_rhy=forgetful
-  // const API = `https://api.datamuse.com/words?rel_rhy=${word}`;
+  const API = `https://api.datamuse.com/words?rel_rhy=${word}`;
     useEffect(() => {
-      // axios.get(API).then(res => {
-      //   setWordsThatRhyne(res.data)
-      // })
-      setWordsThatRhyne(data)
+      axios.get(API).then(res => {
+        setWordsThatRhyne(res.data)
+      })
+      // setWordsThatRhyne(data)
     }, []);
 
-  const parseWordsThatRhyme = Array.isArray(wordsThatRhyme) && wordsThatRhyme.map(rhymeWord => (
-    <li>{rhymeWord.word}</li>
+  const parseWordsThatRhyme = Array.isArray(wordsThatRhyme) && wordsThatRhyme.map((rhymeWord, index) => (
+    <li key={index}>{rhymeWord.word}</li>
   ));
   
   return (
