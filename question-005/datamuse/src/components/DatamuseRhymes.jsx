1878528765c413d34b82d335b62d72365368c92e
Nick <nsagias@gmail.com>
2022-01-24 02:37:39 -0500

    use mock data to test

--- question-005/datamuse/src/components/DatamuseRhymes.jsx
+++ question-005/datamuse/src/components/DatamuseRhymes.jsx
@@ -1,21 +1,48 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
+const data = [
+    {
+      "word": "fretful",
+      "score": 398,
+      "numSyllables": 2
+    },
+    {
+      "word": "regretful",
+      "score": 302,
+      "numSyllables": 3
+    },
+    {
+      "word": "threatful",
+      "score": 129,
+      "numSyllables": 2
+    }
+  ];
+
 export default function DatamuseRyhmes() {
   const keyWord = "forgetful";
-  const [word, setWord] = useState(keyWord);
-  const [wordThatRhyme, setWordsThatRhyne] = useState([]);
-  console.log(keyWord);
-  console.log(word)
-  console.log(wordThatRhyme)
+  // const [word, setWord] = useState(keyWord);
+  const [wordsThatRhyme, setWordsThatRhyne] = useState([]);
+  // console.log("keyword", keyWord);
+  // console.log("word",word);
+  console.log("world that rhyme",wordsThatRhyme);
 
-  const API = `https://api.datamuse.com/words?rel_rhy=${word}`;
+  // https://api.datamuse.com/words?rel_rhy=forgetful
+  // const API = `https://api.datamuse.com/words?rel_rhy=${word}`;
     useEffect(() => {
-      axios.get(API).then(res => {
-        setWordsThatRhyne(res.data)
-      })
+      // axios.get(API).then(res => {
+      //   setWordsThatRhyne(res.data)
+      // })
+      setWordsThatRhyne(data)
     }, []);
+
+  const parseWordsThatRhyme = Array.isArray(wordsThatRhyme) && wordsThatRhyme.map(rhymeWord => (
+    <li>{rhymeWord.word}</li>
+  ));
+  
   return (
-    <>{wordThatRhyme}</>
+    <ul>
+        {parseWordsThatRhyme}
+    </ul>
   );
 }
