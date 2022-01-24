bb9a393b991c0a04a23eec8ad7d6bc9bb13fbd5a
Nick <nsagias@gmail.com>
2022-01-24 02:17:32 -0500

    Return words that rhyme

--- question-005/datamuse/src/components/DatamuseRhymes.jsx
+++ question-005/datamuse/src/components/DatamuseRhymes.jsx
@@ -1,15 +1,21 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function DatamuseRyhmes() {
   const keyWord = "forgetful";
   const [word, setWord] = useState(keyWord);
   const [wordThatRhyme, setWordsThatRhyne] = useState([]);
+  console.log(keyWord);
+  console.log(word)
+  console.log(wordThatRhyme)
 
   const API = `https://api.datamuse.com/words?rel_rhy=${word}`;
     useEffect(() => {
-      axios.get('')
+      axios.get(API).then(res => {
+        setWordsThatRhyne(res.data)
+      })
     }, []);
   return (
-    <></>
+    <>{wordThatRhyme}</>
   );
 }
